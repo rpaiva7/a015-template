35e794323ed022458e823a78c5f055402504bc5f
Renan Paiva <rpaivadev@gmail.com>
2022-08-17 19:04:03 -0300

    alterações feitas

---
+++ script.js
@@ -0,0 +1,43 @@
+
+/*let num = Number(prompt("Digite um número. Para sair digite zero"))
+let soma = 0
+
+while(num !==0){
+    soma = soma + num
+    num = Number(prompt('Digite um número. Para sair digite zero'))
+}
+
+alert(`Asoma dos números é ${soma}`)*/
+
+
+/*exerc. 2 ------
+
+let num = Number(prompt("Insira um número"))
+
+for(let i = 0; i <= num; i++){
+    console.log(i)
+}*/
+
+//exercício 3
+
+/*const array = [7, 6, 5, 4, 3, 2, 1]
+
+for( let i = 0; i <= array.length; i++)
+{
+    console.log(`O número do índice ${i} é = ${array[i]}`);
+
+}*/
+
+//----exerc. fixação (tabuada)
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let i = 0; i <numeros.length; i++) {
+    console.log(numeros[i]*2);
+}
+
+
+
+
+
+
